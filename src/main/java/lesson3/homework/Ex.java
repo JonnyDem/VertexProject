b9965605708a8c02cc0df1ac956e207e778286b3
JonnyDem <tl-k@ukr.net>
2017-07-08 00:23:43 +0300

    Initial commit

---
+++ src/main/java/lesson3/homework/Ex.java
@@ -0,0 +1,10 @@
+package lesson3.homework;
+
+/**
+ * Created by Ваня on 08.07.2017.
+ */
+public class Ex {
+    public static void main(String[] args) {
+        System.out.println("Hai");
+    }
+}
